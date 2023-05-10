9a45bfcb2790eec91b49cc5b13cc45d78da5b8d4
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-05-09 22:56:35 -0400

    display cards in about section, create section wrapper component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,67 @@ import { motion } from 'framer-motion'
 
 import { styles } from '../styles'
 import { services } from '../constants'
-import { fadeIn, textVariant } from '../utils'
+import { fadeIn, textVariant } from '../utils/motion'
+
+const ServiceCard = ({ index, title, icon}) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn('right', 'spring', 0.5 * index * 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow card'
+        // animate={{ x: 50 }}
+        // transition={{ type: 'spring', stiffness: 100 }}
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 2,
+            speed: 250
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img
+            src={icon} 
+            alt={title} 
+            className='w-16 h-16 object-contain'
+          />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+
+        </div>
+
+      </motion.div>
+    </Tilt>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          Introduction
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Overview.
+        </h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am a full-stack software engineer and a proud graduate of General Assembly's Software Engineering Immersive. My journey in the tech field has been fueled by a blend of analytical thinking, attention to detail, and a creative spirit that loves to innovate.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard 
+            key={service.title}
+            index={index} {...service}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 

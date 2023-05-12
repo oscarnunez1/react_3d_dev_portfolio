27ca4479fc7e7028ded97dbc5c997f8a2687ac11
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-05-12 18:41:34 -0400

    complete Hero and Overview section

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -13,6 +13,9 @@ function HOC() {
       viewport={{ once: true, amount: 0.25 }}
       className={`${styles.padding} max-w-7xl`}
     >
+      <span className="hash-span" id={idName}>
+        &nbsp;
+      </span>
       <Component />
     </motion.section>
   )

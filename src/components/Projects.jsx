b96e5d806cd127f8cde626f913220dc6d228f004
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-05-23 09:42:53 -0400

    place github and demo project card icons bekow the image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,65 +8,67 @@ import { projects } from "../constants"
 import { fadeIn, textVariant } from "../utils/motion"
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link, demo_link }) => {
-return (
-  <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-    <Tilt
-      options={{
-        max: 45,
-        scale: 1,
-        speed: 450
-      }}
-      className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
-    >
-      <div className="relative w-full h-[230px]">
-        <img 
-          src={image}
-          alt={name}
-          className="w-full h-full object-cover rounded-2xl"
-        />
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+      >
+        <div className="relative w-full h-[230px]">
+          <img 
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+        </div>
 
-        <div className="absolute inset-0 flex justify-end m-3 card-img_hover gap-1">
-          <div
-            onClick={() => window.open
-            (source_code_link, "_blank")}
-            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-          >
-            <img 
-              src={github} 
-              alt="github"
-              className="w-full h-full object-contain"
-            />
-          </div>
-          <div // chnage this to project demo
-            onClick={() => window.open
-            (demo_link, "_blank")}
-            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-          >
-            <img 
-              src={demo} 
-              alt="github"
-              className="w-full h-full object-contain"
-            />
+        <div className="flex justify-between items-center mt-5 mb-3">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          
+          <div className="flex gap-2">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img 
+                src={github} 
+                alt="github"
+                className="w-full h-full object-contain"
+              />
+            </div>
+            <div 
+              onClick={() => window.open(demo_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img 
+                src={demo} 
+                alt="demo"
+                className="w-full h-full object-contain"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="mt-5">
-        <h3 className="text-white font-bold text-[24px]">{name}</h3>
-        <p className="mt-2 text-secondary ">{description}</p>
-      </div>
+        <div>
+          <p className="mt-2 text-secondary">{description}</p>
+        </div>
 
-      <div className="mt-4 flex flex-wrap gap-2">
-        {tags.map((tag) => (
-          <p key={tag.name} className={`text-14px] ${tag.color}`}>
-            #{tag.name}
-          </p>
-        ))}
-      </div>
-    </Tilt>
-  </motion.div>
-)
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-14px ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  )
 }
+
 
 const Projects = () => {
   return (

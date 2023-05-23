7b5e5ba26ec15ceaa750c0f0af7934f24e850ba5
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-05-23 11:48:25 -0400

    disable orbit controls

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -52,17 +52,15 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop='demand'
       shadows
-      camera={{ position: [20, 4, 5], fov: 40 }}
+      camera={{ position: [22, 4, 5], fov: 40 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        {!isMobile && (
-          <OrbitControls 
-            enableZoom={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-          />
-        )}
+        {/* <OrbitControls 
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        /> */}
         <Computers isMobile={isMobile} />
       </Suspense>
 

8b3c91f91e457ea603f7e233a6c2247638ed39ff
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-05-30 13:28:25 -0400

    remove the keys console logs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,11 +22,6 @@ const Contact = () => {
     e.preventDefault()
     setloading(true)
 
-    console.log("SERVICE KEY", import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
-    console.log("TEMPLAT KEY", import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID);
-    console.log("PUBLIC KEY", import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
-
-
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
